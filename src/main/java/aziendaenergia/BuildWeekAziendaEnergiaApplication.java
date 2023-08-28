b8d2268a2010b505ab8eb88141ed1d1b6ee97c8b
lucawar <lucaguerra92@yahoo.it>
2023-08-28 12:40:27 +0200

    progetto iniziale

---
+++ src/main/java/aziendaenergia/BuildWeekAziendaEnergiaApplication.java
@@ -0,0 +1,13 @@
+package aziendaenergia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildWeekAziendaEnergiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildWeekAziendaEnergiaApplication.class, args);
+	}
+
+}
